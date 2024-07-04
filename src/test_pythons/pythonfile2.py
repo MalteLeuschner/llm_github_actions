4629730a07964f95ce02707ac6b7be3bf32546fb
leuschnm <malteleuschner@gmx.de>
2024-07-04 22:30:17 +0200

    test file changes

--- src/test_pythons/pythonfile2.py
+++ src/test_pythons/pythonfile2.py
@@ -1,16 +1,4 @@
-def double(number: int) -> None:
-    """: Returns the double of the input number.
-    :
-    : This function takes a number as input and returns its double.
-    :
-    : Args:
-    :   number (int): The number to be doubled.
-    :
-    : Returns:
-    :   int
-    :
-    : Raises:
-    :   TypeError: If the input is not a number."""
+def double(number: int)
     return number * 2
 
 
