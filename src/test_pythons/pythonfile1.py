befc40f0eb9c34b6e3111b746edaaff31ee743e1
leuschnm <malteleuschner@gmx.de>
2024-07-04 11:58:46 +0200

    test file changes

--- src/test_pythons/pythonfile1.py
+++ src/test_pythons/pythonfile1.py
@@ -1,19 +1,5 @@
 import os
-
-
 def main(test):
-    """
-    This is an example Docstring
-    
-    Args:
-        a: an arg
-        
-    Returns:
-        b: some results
-    
-    Example:
-        A very good Example.
-"""
     return test
 
 
