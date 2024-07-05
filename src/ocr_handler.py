791fa03c8bc0d4a5a4b38f503e6342f7471a75e2
leuschnm <malteleuschner@gmx.de>
2024-07-05 12:36:21 +0200

    test file changes

---
+++ src/ocr_handler.py
@@ -0,0 +1,20 @@
+import pytesseract
+from PIL import Image
+
+class OCRHandler:
+    def __init__(self, tesseract_cmd):
+        pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
+
+    def perform_ocr(self, image_path):
+        image = Image.open(image_path)
+        text = pytesseract.image_to_string(image)
+        return text
+
+def main():
+    ocr = OCRHandler(r'/usr/bin/tesseract')
+    text = ocr.perform_ocr('balance_sheet.png')
+    print(text)
+
+if __name__ == '__main__':
+    main()
+
